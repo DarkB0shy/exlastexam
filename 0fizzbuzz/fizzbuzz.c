3ab9c0c059dd4f4f5e0b4ade97086430b70c2005
Davide Carassiti <dcarassi@e3r7p8.42roma.it>
2022-11-03 15:10:10 +0100

    no more a.out

--- 0fizzbuzz/fizzbuzz.c
+++ 0fizzbuzz/fizzbuzz.c
@@ -1,32 +1,26 @@
 #include <unistd.h>
 
-void	write_numba(int i)
+void	put_nbr(int	i)
 {
 	if (i > 9)
-		write_numba(i / 10);
-	i = i % 10;
-	i = i + '0';
-	write(1, &i, 1);
+		put_nbr(i / 10);
+	i = (i % 10) + '0';
+	write (1, &i, 1);
 }
 
 int	main (void)
 {
 	int	i = 1;
 
-	while (i <= 100)
+	while (i < 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			write(1, "fizzbuzz", 8);
-		else if (i % 3 == 0)
+		if (i % 3 == 0)
 			write(1, "fizz", 4);
-		else if (i % 5 == 0)
+		if (i % 5 == 0)
 			write(1, "buzz", 4);
-		else
-		{
-			write_numba(i);
-		}
+		if (i % 3 != 0 && i % 5 != 0)
+			put_nbr(i);
+		write (1, "\n", 1);
 		i++;
-		write(1, "\n", 1);
 	}
-	return (0);
 }
--- 2ft_repeat_alpha/ft_repeat_alpha.c
+++ 2ft_repeat_alpha/ft_repeat_alpha.c
@@ -3,5 +3,21 @@
 
 int	main()
 {
-	char *alpha = ""
+	int	i = 0;
+	int	repeat;
+	char	*alpha = "AbCdEfGhIjKlMnOpQrStUvWxYz";
+	while (alpha[i] != '\0')
+	{
+		repeat = 1;
+		if (alpha[i] >= 'a' && alpha[i] <= 'z')
+			repeat = repeat + alpha[i] - 'a';
+		else if (alpha[i] >= 'A' && alpha[i] <= 'Z')
+			repeat = repeat + alpha[i] - 'A';
+		while (repeat)
+		{
+			write(1, &alpha[i], 1);
+			repeat = repeat - 1;
+		}
+		i = i + 1;
+	}
 }
